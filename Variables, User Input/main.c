a8fccc38019b88ffd87bcd44e60e61f0e149d3ce
Isteker Ahmed Siam <istekersiam@gmail.com>
2026-01-25 22:43:39 +0600

    Add files via upload

---
+++ Variables, User Input/main.c
@@ -0,0 +1,9 @@
+// //========== RESTAURANT REVIEW ==========
+
+// I visited [restaurant name] with [number] friends.
+// We ordered the [food item] which cost $[price].
+// The food was [adjective1] and [adjective2]!
+// Our waiter [waiter's name] gave us [rating] star service.
+// Overall rating: [rating]/5.0 stars
+
+// ========================================
