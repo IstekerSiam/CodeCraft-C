8834196ec8aff48fd9aad15df9ed0c874d37dd21
Isteker Ahmed Siam <istekersiam@gmail.com>
2026-01-31 21:02:41 +0600

    Delete Variables, User Input directory

--- Variables, User Input/main.c
+++
@@ -1,9 +0,0 @@
-// //========== RESTAURANT REVIEW ==========
-
-// I visited [restaurant name] with [number] friends.
-// We ordered the [food item] which cost $[price].
-// The food was [adjective1] and [adjective2]!
-// Our waiter [waiter's name] gave us [rating] star service.
-// Overall rating: [rating]/5.0 stars
-
-// ========================================
